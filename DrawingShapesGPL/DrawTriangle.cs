64dde266ed9b3b502bf642f73abe96d19a63c3c9
manoj karki <manojkarki2020@outlook.com>
2020-01-12 19:47:00 +0545

    Add function to check triangle existence. checkTriangleValidity

--- DrawingShapesGPL/DrawTriangle.cs
+++ DrawingShapesGPL/DrawTriangle.cs
@@ -10,33 +10,51 @@ namespace DrawingShapesGPL
     {
         /// <summary>The width of triangle.</summary>
         private int tWidth;
-        
+
         /// <summary>The height of triangle.</summary>
         private int tHeight;
-        
+
         /// <summary>The hypotenuse of triangle.</summary>
         private int tHypotenuse;
 
-        
+        /// <summary>Checks whether triangle exists or not.</summary>
+        /// <returns>True || False</returns>
+        public Boolean checkTriangleValidity()
+        {
+            // check condition 
+            if (tWidth + tHeight <= tHypotenuse || tWidth + tHypotenuse <= tHeight || tHeight + tHypotenuse <= tWidth)
+                return false;
+            else
+                return true;
+        }
+
         /// <summary>Draws the Triangle shape.</summary>
         /// <param name="g">The g.</param>
         /// <exception cref="DrawingShapesGPL.DrawingShapesException">Triangle doesnt exist.</exception>
         public void DrawShape(Graphics g)
         {
-            Pen myPen = new Pen(Color.Black, 4);
-            Point[] pnt = new Point[3];
+            if (checkTriangleValidity())
+            {
+                Pen myPen = new Pen(Color.Black, 4);
+                Point[] pnt = new Point[3];
 
-            pnt[0].X = mouseX;
-            pnt[0].Y = mouseY;
-            
-            pnt[1].X = mouseX - tWidth;
-            pnt[1].Y = mouseY;
+                pnt[0].X = mouseX;
+                pnt[0].Y = mouseY;
 
-            pnt[2].X = mouseX;
-            pnt[2].Y = mouseY - tHeight;
-            g.DrawPolygon(myPen, pnt);
+                pnt[1].X = mouseX - tWidth;
+                pnt[1].Y = mouseY;
+
+                pnt[2].X = mouseX;
+                pnt[2].Y = mouseY - tHeight;
+                g.DrawPolygon(myPen, pnt);
+            }
+            else
+            {
+                throw new DrawingShapesException("Triangle doesnt exist.");
+            }
+
         }
-        
+
         /// <summary>Sets the parameters of the triangle.</summary>
         /// <param name="width">The width.</param>
         /// <param name="height">The height.</param>
