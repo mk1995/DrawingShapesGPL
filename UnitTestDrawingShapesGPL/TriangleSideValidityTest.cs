e502e7b0194eabf7a86708b9bd113088255223af
manoj karki <manojkarki2020@outlook.com>
2020-01-12 20:26:22 +0545

    Performing unit test for triangle existence

---
+++ UnitTestDrawingShapesGPL/TriangleSideValidityTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using DrawingShapesGPL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestDrawingShapesGPL
+{
+    [TestClass]
+    public class TriangleSideValidityTest
+    {
+        [TestMethod]
+        public void checkTriangleValidity()
+        {
+            DrawTriangle dt = new DrawTriangle();
+            dt.SetShapeParam(1, 2, 3);
+            bool expectedResult = false;
+            bool actualResult = dt.checkTriangleValidity();
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
