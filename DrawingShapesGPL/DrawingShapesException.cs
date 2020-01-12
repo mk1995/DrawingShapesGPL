2311529d0ef664c072116e5d2f5de62bd48911cc
manoj karki <manojkarki2020@outlook.com>
2020-01-12 19:41:51 +0545

    Adding User defined exception . DrawingShapesException

---
+++ DrawingShapesGPL/DrawingShapesException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawingShapesGPL
+{
+    /// <summary>Custom defined exception for DrawiingShapes</summary>
+    /// <seealso cref="System.Exception" />
+    public class DrawingShapesException : Exception
+    {
+        /// <summary>Initializes a new instance of the <see cref="DrawingShapesException"/> class.</summary>
+        /// <param name="msg">The error message.</param>
+        public DrawingShapesException(string msg) : base(String.Format("DrawingShapesException: {0}", msg))
+        { }
+    }
+}
